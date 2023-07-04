6e1c59da8985bfaf56128e6e505dd83de4c6d9b9
316de <316de@315-DE-20>
2023-07-04 11:44:40 +0300

    Добавьте файлы проекта.

---
+++ Библиотека_экзамен/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Библиотека_экзамен
+{
+    public class Buildig
+    {
+        //Создание полей класса
+        public string Adress { get; set; }//Адрес
+        public string Description { get; set; } //Описание
+        public List<Apartments> Apartments = new List<Apartments>();//Список квартир (связан с соответствующим классом)
+
+        //Создание объекта класса
+        public Buildig(string adress, string description)
+        {
+            Adress = adress;
+            Description = description;
+        }
+        //Создание метода класса, показывающего все здания и их описание
+        public void ShowAll()
+        {
+            Console.WriteLine("Адрес здания - " + Adress + "  Описание здания " + Description);
+        }
+
+        //С гордостью и надменностью выполнила ГУЛЕНОК АРИНА 22ИС
+    }
+    //Создание дополнительного класса
+    public class Apartments
+    {
+        public string Owner { get; set; }//Владелец квартиры
+        public int Number { get; set; }//Номер квартиры
+        public Apartments(string owner, int number)//Создание объекта класса
+        {
+            Owner = owner;
+            Number = number;
+        }
+        //Создание метода, показывающего все номера квартир и их владельцев
+        public void Show()
+        { Console.WriteLine("Владелец квартиры - " + Owner + "  Номер квартиры - " + Number); }
+
+    }
+}
---
+++ Консольное_приложение_экзамеен/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Библиотека_экзамен;
+
+namespace Консольное_приложение_экзамеен
+{
+    //Со слезами и отчаянием выполнила ГУЛЕНОК АРИНА 22ИС
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создание описания объектов основного класса
+            Buildig South_Park = new Buildig("Северная улица, дом 22", "Новый дом в молодом районе, прекрасный выбор для семьи с детьми");
+            Buildig Warhsawa = new Buildig("Улица Дэвида Боуи, дом 94", "Историческое здание в центре города. Отличный выбор для творческого человека");
+            Buildig BorderLine = new Buildig("Улица Бумажная, дом 154", "Современное здание в бизнес-центре. Прекрасный вариант для работников IT-сферы");
+            //Создание описания объектов дополнительного класса
+            Apartments pers_1 = new Apartments("Новиков Марк Михайлович", 132);
+            Apartments pers_2 = new Apartments("Викторов Александр Андеевич", 42);
+            Apartments pers_3 = new Apartments("Спухня Анна Владимировна", 366);
+            //Добавление объектов дополнительного класса в список объектов основного класса
+            BorderLine.Apartments.Add(pers_1);
+            Warhsawa.Apartments.Add(pers_2);
+            Warhsawa.Apartments.Add(pers_3);
+            //Вывод на экран информации о зданиях и квартирах
+            //Первое здание. Вывод информации о здании и о жильцах
+            South_Park.ShowAll();
+            Console.WriteLine("Жильцы в доме:");
+            foreach (var pers in South_Park.Apartments)
+            {
+                pers.Show();
+            }
+            //Второе здание. Вывод информации о здании и о жильцах
+            BorderLine.ShowAll();
+            Console.WriteLine("Жильцы в доме:");
+            foreach (var pers in BorderLine.Apartments)
+            {
+                pers.Show();
+            }
+            //Третье здание. Вывод информации о здании и о жильцах
+            Warhsawa.ShowAll();
+            Console.WriteLine("Жильцы в доме:");
+            foreach (var pers in Warhsawa.Apartments)
+            {
+                pers.Show();
+            }
+            //Со слезами и отчаянием выполнила ГУЛЕНОК АРИНА 22ИС
+            Console.ReadKey();
+        }
+    }
+}
